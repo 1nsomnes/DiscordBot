c023b40e4fcba7e4a9b7be32ef72a7a2187697be
1nsomnes <insomnes@protonmail.com>
2020-10-29 11:03:52 -0700

    Added Database Model and Service

---
+++ DiscordBot/Core/Database/DatabaseModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+
+namespace DiscordBot.Core.Database
+{
+    public class DatabaseModel : DbContext
+    {
+
+        public DatabaseModel()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Infraction> Infractions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=infractions.db");
+        }
+    }
+
+    public class Infraction
+    {
+        public ulong id { get; set; }
+        public ulong userId { get; set; }
+
+        public string severity { get; set; }
+        public string description { get; set; }
+
+        public DateTime creationDate { get; set; }
+        public DateTime modificationDate { get; set; }
+
+        public ulong modId { get; set; }
+    }
+}
---
+++ DiscordBot/Core/Database/DatabaseService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordBot.Core.Database
+{
+    public class DatabaseService
+    {
+        public static void AddInfraction(ulong userId, string severity, string description, ulong modId)
+        {
+            using (var context = new DatabaseModel())
+            {
+                Infraction infraction = new Infraction();
+                infraction.userId = userId;
+                infraction.severity = severity;
+                infraction.description = description;
+                infraction.modId = modId;
+
+                infraction.id = context.Infractions.AsQueryable().Select(p => p.id).OrderBy(p => p).Last() + 1;
+                infraction.creationDate = DateTime.UtcNow;
+                infraction.modificationDate = DateTime.UtcNow;
+
+                var entity = context.Infractions.Add(infraction);
+                entity.State = EntityState.Added;
+
+                context.SaveChanges();
+            }
+        }
+
+        public static void RemoveInfraction(ulong infractionId)
+        {
+            using (var context = new DatabaseModel())
+            {
+                var result = context.Infractions.AsQueryable().Where(inf => inf.id == infractionId).First();
+
+                if (result is null) return;
+
+                var entity = context.Infractions.Remove(result);
+                entity.State = EntityState.Deleted;
+
+                context.SaveChanges();
+            }
+        }
+
+        public static void EditInfraction(ulong infractionId, string description)
+        {
+            using (var context = new DatabaseModel())
+            {
+                var result = context.Infractions.AsQueryable().Where(inf => inf.id == infractionId).First();
+
+                if (result is null) return;
+
+                result.description = description;
+
+                var entity = context.Infractions.Update(result);
+                entity.State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
